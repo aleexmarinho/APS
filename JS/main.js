d5da3f9e7cbb003f15cd338bf24a277884a4d4d8
Alexson Marinho <alexsonmartinsmarinho@gmail.com>
2023-05-24 22:51:08 -0300

    APS ÁREAS VERDES

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+(function(){
+    var $body = document.querySelector('body')
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+
+    var $btnMenu = document.querySelector('.header__btnMenu');
+    $btnMenu.removeAttribute('style')
+})()
